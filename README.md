aa501842c550bab2d149365d84204b650842640f
Nader Nasr <115250714+NaderNasr99@users.noreply.github.com>
2023-01-15 23:27:23 +0200

    Add files via upload

---
+++ README.md
@@ -0,0 +1,16 @@
+# Educast
+A model made specifically for Educast By Nader Nasr(https://nadernasr99.github.io/Portfolio/)
+## Used:
+
+* HTML.
+* CSS.
+* SCSS.
+* JavaScript.
+* JQuery.
+
+## Silent Features.
+
+* Responsive Design.
+* Portfolio Template.
+* Unique and Dynamic Design.
+* Contains HOME,EPISODES, ABOUT US, CONTACT and many other sections.
---
+++ js/main.js
@@ -0,0 +1,61 @@
+/*  ---------------------------------------------------
+    Template Name: Educast
+    Version: 1.0
+    Created: Nader Nasr
+---------------------------------------------------------  */
+
+'use strict';
+
+(function ($) {
+
+    /*------------------
+        Preloader
+    --------------------*/
+    $(window).on('load', function () {
+        $(".loader").fadeOut();
+        $("#preloder").delay(200).fadeOut("slow");
+    });
+    /*------------------
+        Background Set
+    --------------------*/
+    $('.set-bg').each(function () {
+        var bg = $(this).data('setbg');
+        $(this).css('background-image', 'url(' + bg + ')');
+    });
+
+    /*------------------
+		Navigation
+	--------------------*/
+    $(".mobile-menu").slicknav({
+        prependTo: '#mobile-menu-wrap',
+        allowParentLinks: true
+    });
+
+    //Canvas Menu
+    $(".canvas__open").on('click', function () {
+        $(".offcanvas-menu-wrapper").addClass("active");
+        $(".offcanvas-menu-overlay").addClass("active");
+    });
+
+    $(".offcanvas-menu-overlay").on('click', function () {
+        $(".offcanvas-menu-wrapper").removeClass("active");
+        $(".offcanvas-menu-overlay").removeClass("active");
+    });
+})(jQuery);
+
+// toTop===========
+let mybutton = document.getElementById("myBtn");
+window.onscroll = function() {scrollFunction()};
+
+function scrollFunction() {
+  if (document.body.scrollTop > 250 || document.documentElement.scrollTop > 250) {
+    mybutton.style.display = "block";
+  } else {
+    mybutton.style.display = "none";
+  }
+}
+function topFunction() {
+  document.body.scrollTop = 0; 
+  document.documentElement.scrollTop = 0; 
+}
+
